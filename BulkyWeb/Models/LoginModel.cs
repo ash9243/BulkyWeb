3bedbdfec96981950aca4a1f8788608d7756d7e8
Apple <apple@Apples-MacBook-Air.local>
2023-08-29 11:53:21 +0530

    First Commit 1. Two buttons Added - Login & Registration 2. Login Page UI complete Bussiness Logic Remaining

---
+++ BulkyWeb/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BulkyWeb.Models
+{
+	public class LoginModel
+	{
+		public LoginModel()
+		{
+		}
+
+		public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
+
